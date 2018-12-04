86f98c61c431ff31366af863f150b7295f50b86b
José Miguel Chávez <43799628+MchSkull@users.noreply.github.com>
2018-12-04 10:05:11 -0600

    Add files via upload

---
+++ Practica4/practica4 tab Controller/practica4 tab Controller/Formulario.swift
@@ -0,0 +1,35 @@
+//
+//  Formulario.swift
+//  practica4 tab Controller
+//
+//  Created by I104-26 on 18/09/18.
+//  Copyright © 2018 I104-26. All rights reserved.
+//
+
+import UIKit
+
+class Formulario: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Practica4/practica4 tab Controller/practica4 tab Controller/lista.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  practica4 tab Controller
+//
+//  Created by I104-26 on 18/09/18.
+//  Copyright © 2018 I104-26. All rights reserved.
+//
+
+import UIKit
+
+class lista: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
